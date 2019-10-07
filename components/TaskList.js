ec90ac3c5ce86cf812d62757e84c06821dd0f85a
Bibiane <bmorakot@macalester.edu>
2019-10-07 10:03:03 -0500

    Update TaskList.js

---
+++ components/TaskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function TaskList() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = {
+  container: {
+    paddingTop: 24
+  }
+}
+
+export default TaskList;
