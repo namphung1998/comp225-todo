f335175fe16b76a4778567da1a7ab8266fa89b38
Nam Phung <nphung@macalester.edu>
2019-10-07 09:56:55 -0500

    added progress container

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ProgressScreen from './screens/ProgressScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <ProgressScreen/>
     </View>
   );
 }
---
+++ screens/ProgressScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ProgressScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = {
+  container: {
+    paddingTop: 24
+  }
+}
+
+export default ProgressScreen;
