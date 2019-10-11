908d1d814cbd14f76208567070f53104ebac089d
Bibiane <bmorakot@macalester.edu>
2019-10-11 18:22:45 -0500

    Updates to AddTask

--- screens/AddTask.js
+++ screens/AddTask.js
@@ -1,13 +1,58 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button} from 'react-native';
 
 function AddTask() {
-  const [value, onChangeText] = React.useState("Enter Task Name Here")
-  return(
-    <View>
-      <Text>This is working.</Text>
+  const [enteredTask, setEnteredTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const taskInputHandler = (enteredTask) => {
+    setEnteredTask(enteredTask);
+  };
+
+  const addTaskHandler = () => {
+    setTasks(currentTasks => [...currentTasks, enteredTask]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Task Name"
+          style={styles.input}
+          onChangeText={taskInputHandler}
+          value={enteredTask}
+        />
+        <Button title="ADD" onPress={addTaskHandler}/>
+      </View>
+      <View>
+        {tasks.map((task) =>
+          <View><Text key={task}>{task}</Text></View>)}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default AddTask;
