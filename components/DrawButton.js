4e4214ec47386c58be9f2817f66736a992f848df
Linda <ylu@macalester.edu>
2019-11-06 16:21:33 -0600

    draw button

---
+++ components/DrawButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+function DrawButton(props) {
+  return (
+    <TouchableOpacity style={[styles.container, props.style]} onPress={props.onPress}>
+      <View>
+        {props.children}
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = {
+  container: {
+    position: 'absolute',
+    bottom: 24,
+    right: 24,
+    height: 72,
+    width: 72,
+    borderRadius: 144,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+};
+
+export default DrawButton;
